5da890f9506a8a2be4d35117294ddf592ad0fb5b
Schemsou <chamsounounou7@mail.com>
2022-07-15 17:31:33 +0100

    first version

--- EmailsScraper.py
+++ EmailsScraper.py
@@ -1,1 +1,23 @@
-print("hello")
+from bs4 import BeautifulSoup as btsp
+import requests, re
+import pandas as pd
+
+
+email_id = re.compile("\w+@\w+.\w+.")
+html = requests.get('https://webspotter.io/blackrock-email-format-hn8a5n').text
+soup = btsp(html, "html.parser")
+
+emails = soup.find_all(text=email_id)
+
+#creating an empty list to add emails into it
+emails_list = []
+
+for email in emails:
+    emails_list.append(email)
+
+#creating a dataframe with pandas and pass our data to it
+data = pd.DataFrame(emails_list)
+
+#storing data in an excel file
+data.to_excel("emails_list.xlsx")
+
